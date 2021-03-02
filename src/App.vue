f042f8923bf70f285f4b83d692f8f19d3b36f9f4
Teague Murray-Marshall <t.murraymarshall@student.fontys.nl>
2021-03-02 15:56:29 +0100

    Add Buefy, Router, Home WIP

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div id="app" class="container">
+  <b-navbar>
+   <template #start>
+    <b-navbar-item href="#">
+     Home
+    </b-navbar-item>
+    <b-navbar-item href="#">
+     Documentation
+    </b-navbar-item>
+    <b-navbar-dropdown label="Info">
+     <b-navbar-item href="#">
+      About
+     </b-navbar-item>
+     <b-navbar-item href="#">
+      Contact
+     </b-navbar-item>
+    </b-navbar-dropdown>
+   </template>
+   <template #end>
+    <b-navbar-item tag="div">
+     <div class="buttons">
+      <a class="button is-primary">
+       <strong>Sign up</strong>
+      </a>
+      <a class="button is-light">
+       Log in
+      </a>
+     </div>
+    </b-navbar-item>
+   </template>
+  </b-navbar>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+ <mission-overview></mission-overview>
+</template>
+
+<script>
+import MissionOverview from '@/components/MissionOverview'
+
+export default {
+ name: 'Home',
+ components: MissionOverview,
+}
+</script>
+
+<style>
+
+</style>
